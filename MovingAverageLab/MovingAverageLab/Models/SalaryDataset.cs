eb5d78ecdae633168e0eb927e2b75df423c5dadf
Ренат Хисматулин <hismatulinrenat20@gmail.com>
2026-05-18 01:31:56 +0300

    Data model classes have been created

---
+++ MovingAverageLab/MovingAverageLab/Models/SalaryDataset.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Коллекция записей. Инкапсулирует список и предоставляет методы доступа
+namespace MovingAverageLab.Models
+{
+    public class SalaryDataset
+    {
+        private List<SalaryRecord> _records;
+        private string _description;
+        public SalaryDataset(List<SalaryRecord> records, string description = "")
+        {
+            _records = records;
+            _description = description;
+        }
+
+        public List<SalaryRecord> Records => _records;
+
+        public List<int> Years => _records.Select(r => r.Year).ToList();
+
+        public List<double> MaleSalaries => _records.Select(r => r.Male).ToList();
+
+        public List<double> FemaleSalaries => _records.Select(r => r.Female).ToList();
+
+        public int Count => _records.Count;
+
+        public string Description => _description;
+    }
+}
---
+++ MovingAverageLab/MovingAverageLab/Models/SalaryRecord.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Одна запись: год + медианная зарплата мужчин и женщин
+namespace MovingAverageLab.Models
+{
+    public class SalaryRecord : BaseRecord
+    {
+        private int _year;
+        private double _male;
+        private double _female;
+
+        public SalaryRecord(int year, double male, double female)
+        {
+            _year = year;
+            _male = male;
+            _female = female;
+        }
+
+        // Инкапсуляция через свойства
+        public int Year => _year;
+
+        public double Male => _male;
+
+        public double Female => _female;
+
+        public double Difference => _male - _female;
+
+        public override Dictionary<string, object> ToDictionary()
+        {
+            return new Dictionary<string, object>
+            {
+                { "year", _year },
+                { "male", _male },
+                { "female", _female }
+            };
+        }
+
+        public override bool Validate()
+        {
+            return (_year >= 2000 && _year <= 2100 && _male > 0 && _female > 0);
+        }
+    }
+}
